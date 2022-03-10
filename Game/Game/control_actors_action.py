844a928ac50d8d970070943940c18b7b807e7043
dragonslayer109 <raydonvetten@gmail.com>
2022-03-10 12:19:01 +0200

    update on classes
    feat - basic methods added
    
    methods added to classes controlActorsActions and director.
    methods passed not functionallity added yet

---
+++ Game/Game/control_actors_action.py
@@ -0,0 +1,13 @@
+import constants
+from action import Action
+from point import Point
+
+class ControlActorsAction(Action):
+    """
+    Input actions that control the players movements and actions
+    """
+    def __init__(self, keyboard_service):
+    pass
+
+    def execute(self, cast, script):
+    pass
---
+++ Game/Game/director.py
@@ -0,0 +1,10 @@
+class Director:
+
+    def __init__(self, video_service):
+        pass
+
+    def start_game(self, cast, script):
+        pass
+
+    def _execute_actions(self, group, cast, script):
+        pass
