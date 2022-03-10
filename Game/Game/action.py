bbd56895bc7acf7cd56e35d61a75f1785efcf2a3
dragonslayer109 <raydonvetten@gmail.com>
2022-03-10 12:07:16 +0200

    Update action.py
    feat- action class completed
    logic added to action class with comments.

--- Game/Game/action.py
+++ Game/Game/action.py
@@ -1,9 +1,10 @@
 class Action:
     """
+    Actions that are done by the cast and added as a script
     """
 
     def execute(self, cast, script):
         """
-
+        Executes the specific action
         """
         pass
