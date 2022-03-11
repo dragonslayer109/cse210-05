73eeb860cf1053109f36aafe99ddbceb4b5ad07c
dragonslayer109 <raydonvetten@gmail.com>
2022-03-11 12:26:10 +0200

    Update draw-actors-action.py
    feat - framework added for class with methods.

---
+++ Game/Game/draw-actors-action.py
@@ -0,0 +1,11 @@
+from action import Action
+
+class DrawActorsAction(Action):
+    """
+    """
+    def __init__(self, video_service):
+        pass
+    def execute(self, cast, script):
+        """
+        """
+        pass
