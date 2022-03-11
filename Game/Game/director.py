64db4e1d53800451715aae55122d3f44f057f77f
dragonslayer109 <raydonvetten@gmail.com>
2022-03-11 13:28:23 +0200

    Update director.py
    feat - director class methods and functionality added
    
    Game loop logic added to start_game method.
    
    Iterate through all actions added to execute_actions method.

--- Game/Game/director.py
+++ Game/Game/director.py
@@ -1,10 +1,27 @@
 class Director:
 
     def __init__(self, video_service):
-        pass
+        """
+        
+        """
+        self._video_service = video_service
 
     def start_game(self, cast, script):
-        pass
+        """
+        Run the game loop till game closed
+        """
+        self._video_service.open_window()
+        while self._video_service.is_window_open():
+            self._execute_actions("input", cast, script)
+            self._execute_actions("update", cast, script)
+            self._execute_actions("output", cast, script)
+        self._video_service.close_window()
+
 
     def _execute_actions(self, group, cast, script):
-        pass
+        """
+        Execute the actions based on the group (input update or output)
+        """
+        actions = script.get_actions(group)    
+        for action in actions:
+            action.execute(cast, script)          
