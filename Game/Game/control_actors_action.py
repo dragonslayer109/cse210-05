2d34668701815b16368bae23eb4c7df39b64d656
dragonslayer109 <raydonvetten@gmail.com>
2022-03-11 12:46:18 +0200

    Update control_actors_action.py
    feat -  methods updated with functionality
    
    Keyboard key press added with update to direction of player. Added for both players. Players head direction changed according to logic created

--- Game/Game/control_actors_action.py
+++ Game/Game/control_actors_action.py
@@ -7,7 +7,54 @@ class ControlActorsAction(Action):
     Input actions that control the players movements and actions
     """
     def __init__(self, keyboard_service):
-    pass
+        """
+        """
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
 
     def execute(self, cast, script):
-    pass
+        """
+        Gets the input from the actors(players) controls and updates direction
+        """
+        """
+        Movement for player 1 keyboard controls
+        """
+        # left player 1
+        if self._keyboard_service.is_key_down('a'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+        
+        # right player 1
+        if self._keyboard_service.is_key_down('d'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+        
+        # up player 1
+        if self._keyboard_service.is_key_down('w'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+        
+        # down player 1
+        if self._keyboard_service.is_key_down('s'):
+            self._direction = Point(0, constants.CELL_SIZE)
+        """
+        Movement for player 2 keyboard controls
+        """
+        # left player 2
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+        
+        # right player 2
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+        
+        # up player 2
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+        
+        # down player 2
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
+        
+        player_1 = cast.get_first_actor("player 1")
+        player_1.turn_head(self._direction)
+
+        player_2 = cast.get_first_actor("player 2")
+        player_2.turn_head(self._direction)
